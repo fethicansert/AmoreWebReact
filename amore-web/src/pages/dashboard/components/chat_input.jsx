e4a045eb85701e7f574c5548a001f110c957c261
fethicansert <fethisert0@gmail.com>
2025-04-02 17:41:51 +0300

    working on voive recording

--- amore-web/src/pages/dashboard/components/chat_input.jsx
+++ amore-web/src/pages/dashboard/components/chat_input.jsx
@@ -1,47 +1,107 @@
-import React, { startTransition, useState } from 'react'
-import { MicrophoneIcon, SendGiftIcon, SendImageIcon, SendMessageIcon } from '../../../assets/svg/svg_package';
-import { colors } from '../../../utils/theme';
+import React, { startTransition, useState } from "react";
+import {
+  CrossCloseIcon,
+  MicrophoneIcon,
+  PauseIcon,
+  SendGiftIcon,
+  SendImageIcon,
+  SendMessageIcon,
+} from "../../../assets/svg/svg_package";
+import { colors } from "../../../utils/theme";
 
-const ChatInput = ({ sendText, handleImageChange, setShowGifts, showGifts }) => {
-    const [messageText, setMessageText] = useState('');
-    const [messageTextFocused, setMessageTextFocused] = useState(false);
-    return (
-        <div className='chat-input-container-wrapper'>
+const ChatInput = ({
+  sendText,
+  handleImageChange,
+  setShowGifts,
+  showGifts,
+}) => {
+  const [messageText, setMessageText] = useState("");
+  const [messageTextFocused, setMessageTextFocused] = useState(false);
+  const [isRecording, setIsRecording] = useState(false);
 
-            <div className='chat-send-image-button'>
-                <input
-                    onChange={handleImageChange}
-                    className='chat-image-input'
-                    type='file'
-                    accept="image/*"
-                />
-                <SendImageIcon />
+  console.log(isRecording);
+
+  return (
+    <div className="chat-input-container-wrapper">
+      <div className="chat-send-image-button">
+        <input
+          onChange={handleImageChange}
+          className="chat-image-input"
+          type="file"
+          accept="image/*"
+        />
+        <SendImageIcon />
+      </div>
+
+      <div
+        className="chat-send-gift-button"
+        style={showGifts ? { background: colors.backGround4 } : {}}
+        onClick={() => setShowGifts((prev) => !prev)}
+      >
+        <SendGiftIcon
+          color={showGifts ? colors.backGround3 : colors.iconColor}
+        />
+      </div>
+
+      <div
+        className="chat-input-container"
+        style={{
+          background: isRecording ? "rgba(221, 136, 207, 0.2)" : "none",
+        }}
+      >
+        {!isRecording ? (
+          <input
+            onKeyUp={(e) => (e.key === "Enter" ? handleSendMessage() : null)}
+            className="chat-input"
+            placeholder="Hadi mesaj yaz"
+            value={messageText}
+            onChange={(e) => setMessageText(e.target.value)}
+            onFocus={() => setMessageTextFocused(true)}
+            onBlur={() => setMessageTextFocused(false)}
+          />
+        ) : (
+          <div className="chat-voice-recording">
+            <span>Ses Kaydediliyor...</span>
+            <div className="chat-voice-recording-container">
+              <div className="chat-voice-recording-cancel-button">
+                <CrossCloseIcon width="16px" height="16px" />
+              </div>
+
+              <div></div>
+
+              <div className="chat-voice-recording-pause-button">
+                <div className="chat-voice-recording-pause-button-line"></div>
+                <div className="chat-voice-recording-pause-button-line"></div>
+              </div>
             </div>
+          </div>
+        )}
+        {messageTextFocused || messageText ? (
+          <SendMessageIcon
+            onClick={() => handleSendMessage()}
+            className="chat-input-icon chat-send-message-icon"
+          />
+        ) : (
+          <MicrophoneIcon
+            className="chat-input-icon"
+            color={colors.brand1}
+            onClick={handleVoiceRecording}
+          />
+        )}
+      </div>
+    </div>
+  );
 
-            <div className='chat-send-gift-button' style={showGifts ? { background: colors.backGround4 } : {}} onClick={() => setShowGifts(prev => !prev)}>
-                <SendGiftIcon color={showGifts ? colors.backGround3 : colors.iconColor} />
-            </div>
+  function handleVoiceRecording() {
+    setIsRecording((prev) => !prev);
+  }
 
-            <div className='chat-input-container'>
-                <input
-                    onKeyUp={(e) => e.key === 'Enter' ? handleSendMessage() : null}
-                    className='chat-input'
-                    placeholder='Hadi mesaj yaz'
-                    value={messageText}
-                    onChange={(e) => setMessageText(e.target.value)}
-                    onFocus={() => setMessageTextFocused(true)}
-                    onBlur={() => setMessageTextFocused(false)} />
-                {(messageTextFocused || messageText) ? <SendMessageIcon onClick={() => handleSendMessage()} className='chat-input-icon chat-send-message-icon' /> : <MicrophoneIcon className='chat-input-icon' color={colors.brand1} />}
-            </div>
-        </div>
-    )
+  function handleSendMessage() {
+    if (!messageText) return;
 
-    function handleSendMessage() {
-        if (!messageText) return
+    sendText(messageText);
+    setMessageText("");
+  }
+};
 
-        sendText(messageText);
-        setMessageText('');
-    }
-}
-
-export default ChatInput
+export default ChatInput;
